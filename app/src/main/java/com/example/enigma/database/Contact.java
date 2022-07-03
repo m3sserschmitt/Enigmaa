f438d8a816a3db34300662494d5cdc1800e7bcf3
Romulus Ruja <romulus-emanuel.ruja@tutanota.com>
2022-07-03 22:00:24 +0300

    Added license;

--- app/src/main/java/com/example/enigma/database/Contact.java
+++ app/src/main/java/com/example/enigma/database/Contact.java
@@ -1,3 +1,20 @@
+/*  Enigma - Onion Routing based messaging app.
+    Copyright (C) 2022  Romulus-Emanuel Ruja <romulus-emanuel.ruja@tutanota.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package com.example.enigma.database;
 
 import android.util.Base64;
